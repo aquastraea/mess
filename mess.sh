905525f66acc518714d6383cef76646686a9cb40
aquastraea <43f78381@opayq.com>
2014-08-18 13:01:04 +0800

    modified mess.sh

--- mess.sh
+++ mess.sh
@@ -1,3 +1,5 @@
 #!/bin/sh
 
+# Here $NF standfor the last value in one line. 
+# S[$1] should be a dict that key=$1,vaule=sum(S[$1]) 
 cat ./stat.log | awk '{if($NF>50) {S[$1]+=$4;R[$1]=$NF}}END{for(i in S) print i,S[i],R[i]}' | sort
